9a2a6b78fccf01204b788a6cdb2f20b05d35beba
vasumv <vasumvikram@gmail.com>
2016-06-24 17:24:02 -0700

    fixed mean subtraction?

--- processimages.py
+++ processimages.py
@@ -5,28 +5,14 @@ from PIL import Image
 from path import Path
 from scipy import misc
 import numpy as np
-import random
-import os
-import sys
 
 def parse_args():
     argparser = ArgumentParser()
-    argparser.add_argument("--dataset_path", default="../yashu/datasets/nabirds/")
-    argparser.add_argument("--image_path", default="images/")
-    argparser.add_argument("--save_path", default="datasets/nabirdsresized/")
+    argparser.add_argument("--dataset_path", default="datasets/nabirdsresized/")
+    argparser.add_argument("--image_path", default="")
+    argparser.add_argument("--save_path", default="datasets/nabirdspreprocessed/")
     return argparser.parse_args()
 
-def load_image_paths(dataset_path='', path_prefix=''):
-    paths = {}
-    print dataset_path
-    with open(os.path.join(dataset_path, 'images.txt')) as f:
-        for line in f:
-            pieces = line.strip().split()
-            image_id = pieces[0]
-            path = os.path.join(path_prefix, pieces[1])
-            paths[image_id] = path
-
-    return paths
 
 def mean_sub_and_normalize(img):
     img = img.astype(float)
@@ -34,22 +20,36 @@ def mean_sub_and_normalize(img):
     img /= np.std(img, axis=0)
     return img
 
+
 if __name__ == "__main__":
     args = parse_args()
     datapath = Path(args.dataset_path)
     imgpath = datapath + Path(args.image_path)
     savepath = Path(args.save_path)
     print len(imgpath.dirs())
+    i = 0
+    image_sum = 0
     for species in imgpath.dirs():
+        print species.name
         if not Path(savepath + species.name).exists():
                 print "Making new directory..."
                 newdir = savepath + species.name
                 newdir.mkdir()
-        else:
-            print "Skipping " + species.name
-            continue
+        for img in species.files():
+            i += 1
+            image = misc.imread(img)
+            image_sum += image.mean()
+    mean = image_sum * 1.0 / i
+    print "Number of images: %d", i
+    print "Sum of images: %s", image_sum
+    print "Mean: %f", mean
+    for species in imgpath.dirs():
         for img in species.files():
             print img.name
-            image = Image.open(img)
-            image = image.resize((256, 256), Image.ANTIALIAS)
-            image.save(savepath + species.name + "/" + img.name)
+            image = misc.imread(img)
+            image = image.astype(float)
+            image -= mean
+            misc.imsave(savepath + species.name + "/" + img.name, image)
+            # image = Image.open(img)
+            # image = image.resize((240, 320), Image.ANTIALIAS)
+            # image.save(savepath + species.name + "/" + img.name)
--- saliencysample.py
+++ saliencysample.py
@@ -1,15 +1,16 @@
 from matplotlib import pyplot as plt
 from argparse import ArgumentParser
 from path import Path
-from scipy import misc
 import numpy as np
 
 
 def parse_args():
     argparser = ArgumentParser()
-    argparser.add_argument("--dataset_path", default="../../tianyiwu/saliency-2016-cvpr/saliency_map/0342/1629aa0f4fa04f88aade70529692e841.png")
+    argparser.add_argument("--dataset_path",
+                           default="../../tianyiwu/saliency-2016-cvpr/saliency_map/")
     argparser.add_argument("--image_path", default="images/")
-    argparser.add_argument("--save_path", default="saliency_maps/0342/20samples/1629aa0f4fa04f88aade70529692e841")
+    argparser.add_argument("--save_path",
+                           default="saliency_map_samples/")
     return argparser.parse_args()
 
 
@@ -21,23 +22,52 @@ def normalize(image):
 
 def sample(image, num):
     nums = np.random.choice(image.size, num, p=image.flatten())
-    row_coords = [num / image.shape[1] for num in nums]
-    col_coords = [num % image.shape[1] - 1 for num in nums]
+    row_coords = [num1 / image.shape[1] for num1 in nums]
+    col_coords = [num1 % image.shape[1] - 1 for num1 in nums]
     return row_coords, col_coords
+
+
+def get_image_id(imagename):
+    return imagename.split(".")[0]
+
+
+def save_sample(rows, cols, savepath):
+    coords = np.vstack((rows, cols))
+    np.save(savepath, coords)
+
+
+def plot_and_save_sample(imagepath, rows, cols, savepath):
+    plot = plt.imread(imagepath)
+    implot = plt.imshow(plot)
+    scat = plt.scatter(x=cols, y=rows, c='r', s=30)
+    plt.savefig(savepath)
+    scat.remove()
 
 
 if __name__ == "__main__":
     args = parse_args()
     datapath = Path(args.dataset_path)
     savepath = Path(args.save_path)
-    if not savepath.exists():
-        savepath.mkdir()
-    saliency_map = misc.imread(datapath)
-    saliency_map = normalize(saliency_map)
-    for i in range(20):
-        rows, cols = sample(saliency_map, 20)
-        plot = plt.imread(datapath)
-        implot = plt.imshow(plot)
-        scat = plt.scatter(x=cols, y=rows, c='r', s=30)
-        plt.savefig(Path(savepath + "/test%d.png") % i)
-        scat.remove()
+    for species in datapath.dirs():
+        print species
+        speciespath = Path(savepath + species.name)
+        if not speciespath.exists():
+            speciespath.mkdir()
+        for img in species.files():
+            with open(savepath + "finishedsamples.txt", "r") as f:
+                text = f.read()
+                finished = set(text.split("\n"))
+            id = get_image_id(img.name)
+            print id
+            if id in finished:
+                print "Skipping %s" % id
+                continue
+            saliency_map = plt.imread(img)
+            saliency_map = normalize(saliency_map)
+            rows, cols = sample(saliency_map, 20)
+            coordssavepath = Path(speciespath + "/" + id + ".npy")
+            # imgsavepath = Path(speciespath + "/" + id + ".png")
+            save_sample(rows, cols, coordssavepath)
+            # plot_and_save_sample(img, rows, cols, imgsavepath)
+            with open(savepath + "finishedsamples.txt", "a") as f:
+                f.write(id + "\n")
